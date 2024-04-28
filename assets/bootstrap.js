40ad1857f79a8155a2582bbc64981772f2e39122
Nadja <ellinger.nadja@gmail.com>
2024-04-28 21:35:56 +0200

    ...

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,7 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { startStimulusApp } from '@symfony/stimulus-bridge';
 
-const app = startStimulusApp();
+// Example of initializing Stimulus with Symfony
+const application = startStimulusApp(require.context('./controllers', true, /\.js$/));
+
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
