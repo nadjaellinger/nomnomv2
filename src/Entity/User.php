f0373928eb4feb91139f76ae15eabecc5663dfcb
Nadja <ellinger.nadja@gmail.com>
2025-04-21 19:56:42 +0200

    Add user approval functionality to admin dashboard and update user roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -81,7 +81,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
-        if (!$this->isApproved) {
+        if (!$this->isApproved && !in_array('ROLE_ADMIN', $roles)) {
             $roles = ['ROLE_PENDING'];
         }
         return array_unique($roles);
